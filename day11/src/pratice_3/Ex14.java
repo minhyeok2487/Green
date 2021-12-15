4a3f02cdda6b4d998dbb428e15f902789764667d
Minhyeok1995 <repeater2487@naver.com>
2021-12-15 18:45:30 +0900

    Update Ex14.java

--- day11/src/pratice_3/Ex14.java
+++ day11/src/pratice_3/Ex14.java
@@ -1,10 +1,35 @@
 package pratice_3;
 
+import java.util.Scanner;
+
 public class Ex14 {
+	public static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		String course[] = {"Java", "C++", "HTML5", "컴퓨터구조", "안드로이드"};
-		int score[] = {95, 88, 76, 62, 55}; 
+		String course[] = { "Java", "C++", "HTML5", "컴퓨터구조", "안드로이드" };
+		int score[] = { 95, 88, 76, 62, 55 };
+		
+		while (true) {
+			int n = 9999;
+			System.out.print("과목 이름>>");
+			String name = scan.nextLine();
+			if (name.equals("그만")) {
+				break;
+			}
+			for (int i = 0; i < course.length; i++) {
+				if (course[i].equals(name)) {
+					n = score[i];
+					System.out.printf("%s의 점수는 %d\n", course[i], n);
+					break;
+				}
+			}
+			if(n==9999) {
+				System.out.println("없는 과목입니다.");
+			}
+			
+
+		}
+
 	}
 
 }
