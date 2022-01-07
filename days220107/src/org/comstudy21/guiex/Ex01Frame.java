5623309ac3692df8b0cccb7c6f3488a2e511e6d6
Minhyeok1995 <repeater2487@naver.com>
2022-01-07 16:40:49 +0900

    2022_01_07

---
+++ days220107/src/org/comstudy21/guiex/Ex01Frame.java
@@ -0,0 +1,13 @@
+package org.comstudy21.guiex;
+
+import java.awt.Frame;
+
+public class Ex01Frame {
+
+	public static void main(String[] args) {
+		Frame frame = new Frame();
+		frame.setSize(640,480);
+		frame.setVisible(true);
+	}
+
+}
