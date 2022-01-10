f7e6a4167edb4b5c51eceea0508e66a7a6a8f699
Minhyeok1995 <repeater2487@naver.com>
2022-01-10 10:21:05 +0900

    Create FrameEvent

--- days220107/src/org/comstudy21/guiex/Ex01Frame.java
+++ days220107/src/org/comstudy21/guiex/Ex01Frame.java
@@ -1,13 +1,15 @@
 package org.comstudy21.guiex;
 
-import java.awt.Frame;
+import java.awt.*;
 
 public class Ex01Frame {
 
 	public static void main(String[] args) {
-		Frame frame = new Frame();
+		Frame frame = new Frame("Hello");
+		frame.setLayout(null);
 		frame.setSize(640,480);
 		frame.setVisible(true);
+		frame.add(new Label("Hello : "));
 	}
 
 }
---
+++ days220107/src/org/comstudy21/guiex/Ex02FrameEvent.java
@@ -0,0 +1,18 @@
+package org.comstudy21.guiex;
+
+import java.awt.*;
+
+public class Ex02FrameEvent extends Frame {
+
+	public Ex02FrameEvent() {
+		setTitle("WindowEventListener 실습");
+		setSize(640,480);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new Ex02FrameEvent();
+		
+	}
+
+}
